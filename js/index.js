7baa3b62f39ec1c4560142b523a95cd57634e0e0
mdsafaet <mdsafaet84@gmail.com>
2024-09-27 20:48:19 +0600

    d

--- js/index.js
+++ js/index.js
@@ -26,7 +26,27 @@ calculatorButton.addEventListener('click',function(){
     result.classList.remove('hidden');
 
     console.table( {totalExpense,balance});
-})
+
+    //history tab
+    
+    const  historyItem = document.createElement('div');
+    historyItem.className = 
+    'bg-white  p-3 rounded-md border-l-2 border-indigo-500';
+
+    historyItem.innerHTML =`
+
+     <p class = 'text-xs text-gray-500'>${new Date().toDateString()}</p>
+     <p class = 'text-xs text-gray-500'>Income: $${income.toFixed(2)}</p>
+     <p class = 'text-xs text-gray-500'>Expense: $${totalExpense.toFixed(2)}</p>
+     <p class = 'text-xs text-gray-500'>Balance: $${balance.toFixed(2)}</p>
+   ` ;
+  
+  const historyContainer =document.getElementById('history-list');
+  historyContainer.appendChild(historyItem,historyContainer.firstChild);
+
+
+});
+
 //add event listener for saving button
 
 const csbtn =document.getElementById('calculate-savings')
@@ -58,3 +78,52 @@ csbtn.addEventListener('click',function(){
 
 });
 //history Tab
+
+const historyTab =document.getElementById('history-tab');
+const assistantTab =document.getElementById('assistant-tab');
+historyTab.addEventListener('click',function(){
+
+    historyTab.classList.add(
+        "text-white",
+        "bg-gradient-to-r",
+        "from-blue-500",
+        "to-purple-600"
+    );
+    historyTab.classList.remove('text-gray-600')
+     assistantTab.classList.remove(
+
+            "text-white",
+            "bg-gradient-to-r",
+            "from-blue-500",
+            "to-purple-600"
+     );
+     historyTab.classList.add('text-gray-600')
+     
+     document.getElementById('expense-form').classList.add('hidden')
+});
+
+
+assistantTab.addEventListener('click',function(){
+    assistantTab.classList.add(
+        "text-white",
+        "bg-gradient-to-r",
+        "from-blue-500",
+        "to-purple-600"
+    );
+
+    
+     historyTab.classList.remove(
+
+            "text-white",
+            "bg-gradient-to-r",
+            "from-blue-500",
+            "to-purple-600"
+     );
+    
+     document.getElementById('expense-form').classList.remove('hidden')
+     document.getElementById('history-section').classList.add('hidden')
+
+     
+
+}) ;
+
